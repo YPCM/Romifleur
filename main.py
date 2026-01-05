e38b07698269d9711f19dd2332f112b80ae1d85b
Mathis B. <109106334+4Sitam4@users.noreply.github.com>
2026-01-06 00:19:19 +0100

    chore: bump version to v1.1.0

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         
-        self.VERSION = "v1.0.7"
+        self.VERSION = "v1.1.0"
 
         # Manager
         self.manager = RomManager()
